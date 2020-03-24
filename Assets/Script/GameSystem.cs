1d0a7eeb19fa659da0078fb82c488859ca80da08
totoko <totoko.staphi@gmail.com>
2020-03-24 13:19:03 +0900

    とげとげの表示（触れると死にます）タイトル画面の表示と遷移。あと背景（宇宙を飛ぶ矢と林檎）

---
+++ Assets/Script/GameSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //スタートボタン挙動
+    public void StartGame(){ //StartGameメソッドは自前のものなので適当な名前に設定
+        SceneManager.LoadScene("SpaceBombR");
+    }
+}
